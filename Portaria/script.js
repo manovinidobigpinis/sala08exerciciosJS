278fc7ccf06eb7960794be91b5415cc3bf622375
fodao <viniestudamuitomesmo@gmail.com>
2026-05-08 00:36:15 -0300

    porra

--- Portaria/script.js
+++ Portaria/script.js
@@ -4,24 +4,30 @@ function check(){
     var clientAge = document.getElementById('age')
     
     if(isNaN(wasBorn)){
-        res.textContent = "Type only numbers 🚫"
+        res.textContent = "Escreve numero bobao 🚫"
         res.style.color = "red"
         return
     }
 
     const thisYear = new Date().getFullYear()
     const age = thisYear - wasBorn
-
+    
     if(age >= 18){
-        res.textContent = "Over 18, entry ✅"
-        clientAge.textContent = age
-        document.getElementById('v1').style.display = 'block'
-        document.getElementById('v2').style.display = 'none'
-    }else{
-        res.textContent = "Under 18, not allowed 🔞"
+        res.textContent = "Mainho de 18✅"
         clientAge.textContent = age
         document.getElementById('v1').style.display = 'none'
-        document.getElementById('v2').style.display = 'block'
+        document.getElementById('v2').style.display = 'none'
+        document.querySelector('.lebron').style.backgroundImage = "url(https://i.pinimg.com/736x/d6/c0/ec/d6c0ece2c8a3420c273076941fda8fcb.jpg)"
+        document.querySelector('.lebron').style.backgroundSize = "1920px 1920px"
+        document.querySelector('.lebron').style.backgroundPosition = "center"
+    }else{
+        res.textContent = "Meno de 18🔞"
+        clientAge.textContent = age
+        document.getElementById('v1').style.display = 'none'
+        document.getElementById('v2').style.display = 'flex'
+        document.querySelector('.lebron').style.backgroundImage = "none"
+        document.querySelector('.lebron').style.backgroundSize = "none"
+        document.querySelector('.lebron').style.backgroundPosition = "none"
     }
 
 }
